439a48dd118adb83cef655e624c88a0719f4fc29
yutaka <ishiabura3@gmail.com>
2020-11-25 02:10:36 +0900

    a

--- Lobook/myapp/migrations/0002_auto_20201115_1412.py
+++ Lobook/myapp/migrations/0002_auto_20201115_1412.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('myapp', '0001_initial'),
     ]
-
+ 
     operations = [
         migrations.AddField(
             model_name='book',
